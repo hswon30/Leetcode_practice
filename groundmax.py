78bfdc77e52f375c50eb8b98c4368ad49e653d44
Hasun Won <glashuitte@gmail.com>
2024-07-16 00:04:44 +0900

    Daily problem solving- programmers groundmax problem minor correction!

--- groundmax.py
+++ groundmax.py
@@ -10,7 +10,8 @@ find best non-overlapping combination of number from each row
 # Since same col cannot be added, and we need to find all global max, add current row to next row to find max
 def solution(land):
     # return max([max(land[row][:col] + land[row][col+1:]) + land[row][col] for row in range(1, len(land)) for col in range(4)])
-    for row in range(len(land)):
+    # note that we need to start from second row as we add onto the second row and downward
+    for row in range(1, len(land)):
 
         # max col size = 4
         for col in range(4):
@@ -21,15 +22,5 @@ def solution(land):
 
     # same as land[len(land)]
     return max(land[-1])
-
-
-land = [[1, 2, 3, 5], [5, 6, 7, 8], [4, 3, 2, 1]]
-for row in range(len(land)):
-
-    # max col size = 4
-    for col in range(4):
-        # list concat and find max [lastrow][col not current]
-        print(land[row][col], land[row - 1][:col], land[row - 1][col + 1:], land[row - 1][:col] + land[row - 1][col + 1:])
-        land[row][col] += max(land[row - 1][:col] + land[row - 1][col + 1:])
 
 
